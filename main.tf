fab9eaa1f0d100ca4d2a48c3e90cc82bf043994c
Avi-Rathod <152466440+Avi-Rathod@users.noreply.github.com>
2024-04-22 00:00:06 +0530

    Update instance.tf
    create instance

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
 provider "aws" {
     region = "ap-south-1"
 }
+resource "aws_instance" "my_instance" {
+    ami = ""
+    instance_type = "t2.micro"
+    key_name = "jenkins"
+    security_groups = ""
+}
